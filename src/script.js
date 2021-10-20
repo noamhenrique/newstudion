bf6814108110f2b67f9bff2948594b648e233249
Noam Henrique <noamhenrique@gmail.com>
2021-10-20 14:13:17 -0300

    feat(homepage): carrossel completed

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 $(document).ready(() => {
   const slickOptions = {
+    autoplay: true,
     dots: false,
+    prevArrow:
+      '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
+    nextArrow:
+      '<button type="button" class="slick-next slider__next-arrow">Previous</button>;',
   };
   $(".slider").slick(slickOptions);
 });
